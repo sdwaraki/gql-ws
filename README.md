375f7068330e0784ddffa831600756e470255534
Sumanth Dwarakinath <sumanth.dwarakinath@gmail.com>
2018-12-09 16:14:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Web service to try out things in GraphQL
+
+Queries
+Mutations
+Connectors
+Error Handling 
+Filtering
+Pagination
---
+++ src/main/java/com/sumanth/projects/gql/gqlws/GqlWsApplication.java
@@ -0,0 +1,12 @@
+package com.sumanth.projects.gql.gqlws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqlWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GqlWsApplication.class, args);
+	}
+}
