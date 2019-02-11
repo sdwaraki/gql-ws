025e8a0f79dd8f254c3a8df9bfe502f26bc9f05f
sumanth <sdwaraki@github.com>
2019-02-11 08:37:49 -0700

    Working with a resolver

--- src/main/java/com/sumanth/projects/gql/gqlws/query/Query.java
+++ src/main/java/com/sumanth/projects/gql/gqlws/query/Query.java
@@ -1,9 +1,8 @@
 package com.sumanth.projects.gql.gqlws.query;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
-import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.sumanth.projects.gql.gqlws.model.Book;
-import com.sumanth.projects.gql.gqlws.repository.BookRepository;
+import com.sumanth.projects.gql.gqlws.resolver.BookResolver;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,14 +10,14 @@ import java.util.List;
 @Component
 public class Query implements GraphQLQueryResolver {
 
-    private final BookRepository bookRepository;
+    private final BookResolver bookResolver;
 
-    public Query(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    public Query(BookResolver bookResolver) {
+        this.bookResolver = bookResolver;
     }
 
     public List<Book> getBooks() {
-        return bookRepository.getBooks();
+        return bookResolver.getBooks();
     }
 
 }
--- src/main/java/com/sumanth/projects/gql/gqlws/resolver/BookResolver.java
+++ src/main/java/com/sumanth/projects/gql/gqlws/resolver/BookResolver.java
@@ -1,9 +1,24 @@
 package com.sumanth.projects.gql.gqlws.resolver;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
-import com.sumanth.projects.gql.gqlws.model.Author;
 import com.sumanth.projects.gql.gqlws.model.Book;
-import com.sumanth.projects.gql.gqlws.repository.AuthorRepository;
+import com.sumanth.projects.gql.gqlws.repository.BookRepository;
+import org.springframework.stereotype.Component;
 
-public class BookResolver implements GraphQLResolver {
+import javax.inject.Inject;
+import java.util.List;
+
+@Component
+public class BookResolver implements GraphQLResolver<Book> {
+
+    @Inject
+    private BookRepository bookRepository;
+
+    void BookResolver(BookRepository repository) {
+        this.bookRepository = repository;
+    }
+
+    public List<Book> getBooks() {
+        return bookRepository.getBooks();
+    }
 }
